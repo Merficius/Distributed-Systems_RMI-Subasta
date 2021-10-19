4560d754914842fc212281d1a7c3e2eb441ca06a
fernandonietoms <fernando.nietoms@gmail.com>
2021-10-19 18:14:56 -0500

    Add Subasta methods from SubastaModelo

--- server/Subasta.java
+++ server/Subasta.java
@@ -1,10 +1,14 @@
 package server;
 
-import java.rmi.Remote;
+import java.util.Vector;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Subasta extends Remote {
-
+    boolean registraUsuario(String nombre) throws RemoteException;
+    boolean agregaProductoALaVenta(String vendedor, 
+        String producto, float precioInicial) throws RemoteException;
+    boolean agregaOferta(String comprador, String producto, float monto) throws RemoteException;
+    Vector obtieneCatalogo() throws RemoteException;
 }
